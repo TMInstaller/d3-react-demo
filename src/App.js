e8b627e5e4a975e0821dd64c85b95f412374ce60
TMInstaller <ajvldmlqjqclr@naver.com>
2023-07-07 14:39:19 +0900

    업데이트

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import HighchartPage from "./HighchartPage";
 
 function App() {
-  const nodes = Array.from({ length: 100 }, (_, i) => ({
-    id: i + 1,
-    name: `Node${i + 1}`,
-  }));
+  const nodes = [
+    { id: "node1", name: "Node 1" },
+    { id: "node2", name: "Node 2" },
+    { id: "node3", name: "Node 3" },
+    { id: "node4", name: "Node 4" },
+    { id: "node5", name: "Node 5" },
+    { id: "node6", name: "Node 6" },
+    { id: "node7", name: "Node 7" },
+    { id: "node8", name: "Node 8" },
+    { id: "node9", name: "Node 9" },
+    { id: "node10", name: "Node 10" },
+  ];
 
-  const links = Array.from({ length: 100 }, (_, i) => ({
-    source: i + 1,
-    target: Math.min(i + 2, 100),
-    name: `Link${i + 1}`,
-  }));
+  const links = [
+    { source: "node1", target: "node2", name: "Link 1-2" },
+    { source: "node1", target: "node3", name: "Link 1-3" },
+    { source: "node1", target: "node4", name: "Link 1-4" },
+    { source: "node2", target: "node3", name: "Link 2-3" },
+    { source: "node2", target: "node4", name: "Link 2-4" },
+    { source: "node2", target: "node5", name: "Link 2-5" },
+    { source: "node3", target: "node4", name: "Link 3-4" },
+    { source: "node3", target: "node5", name: "Link 3-5" },
+    { source: "node3", target: "node6", name: "Link 3-6" },
+    { source: "node4", target: "node5", name: "Link 4-5" },
+    { source: "node4", target: "node6", name: "Link 4-6" },
+    { source: "node4", target: "node7", name: "Link 4-7" },
+    { source: "node5", target: "node6", name: "Link 5-6" },
+  ];
 
   const svgRef = useRef();
   const width = 1600;
